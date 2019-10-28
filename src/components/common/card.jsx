913d95cb86639bf02b930c01e43a607e4769499c
Xingkai Yang <xingkaiyang@Xingkais-MacBook-Pro.local>
2019-10-28 15:44:47 -0400

    card pic add

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import test from '../../imgs/test.jpg';
 
 class Card extends Component {
     state = {}
     render() {
-        const { city, temp } = this.props;
+        // const { city, temp } = this.props;
         return (
 
             <div className="card">
-                <img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+                <img className="card-img-top" src={test} alt="Card image cap" />
                 <div className="card-body">
-                    <h5 className="card-title">{city}</h5>
-                    <p className="card-text">{temp}</p>
+                    <h5 className="card-title">CityName</h5>
+                    <p className="card-text">Temp</p>
+                    <p className="card-text">Temp</p>
+                    <p className="card-text">Temp</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
