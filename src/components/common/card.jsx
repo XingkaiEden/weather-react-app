21e60b5c5c5b560a0303cc2e016f69e79efcce10
Xingkai Yang <xingkaiyang@Xingkais-MacBook-Pro.local>
2019-10-29 14:54:36 -0400

    done pass data to card and rendering a list of cityWeather

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -4,16 +4,16 @@ import test from '../../imgs/test.jpg';
 class Card extends Component {
     state = {}
     render() {
-        // const { city, temp } = this.props;
+        const { cityName, temp } = this.props;
         return (
 
             <div className="card">
-                <img className="card-img-top" src={test} alt="Card image cap" />
+                <img className="card-img-top" src={test} />
                 <div className="card-body">
-                    <h5 className="card-title">CityName</h5>
-                    <p className="card-text">Temp</p>
-                    <p className="card-text">Temp</p>
-                    <p className="card-text">Temp</p>
+                    <h5 className="card-title">{cityName}</h5>
+                    <p className="card-text">{temp}</p>
+                    <p className="card-text">{temp}</p>
+                    <p className="card-text">{temp}</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
--- src/components/weathers.jsx
+++ src/components/weathers.jsx
@@ -15,12 +15,27 @@ class Weathers extends Component {
 
 
 
+    componentDidUpdate(prevProps, prevState) {
 
+    }
     getWeatherData = async cities => {
-        const id = cities[0].id;
+        const id = cities[cities.length - 1].id;
         const { data } = await http.get(`${apiUrl}/data/2.5/weather?id=${id}&appid=${apiKey}`);
         const weathers = [...this.state.weathers, data];
         this.setState({ weathers });
+
+    }
+
+    testDuplicateCity = searchResults => {
+
+        if (this.state.targetCities.length !== 0) {
+            if (this.state.targetCities.find(city => city.id === searchResults[0].id)) {
+                toast.error("City has been addeds");
+                return false;
+            }
+        }
+
+        return true;
 
     }
 
@@ -31,13 +46,23 @@ class Weathers extends Component {
         const searchResults = cities
             .filter(c => c.name.toLowerCase() === cityName.toLowerCase())
             .filter(c => c.country.toLowerCase() === countryName.toLowerCase());
-        if (searchResults.length === 0)
-            toast.error("Invalid City");
 
+        if (searchResults.length === 0) {//did not find the city in the list
+            toast.error("Invalid City");
+            return;
+        }
+
+
+        if (!this.testDuplicateCity(searchResults)) return;
+
+        console.log(this.state.targetCities);
         const targetCities = [...this.state.targetCities, ...searchResults];
 
-        this.setState({ targetCities }, () => { this.getWeatherData(this.state.targetCities) });
-        console.log(this.state.targetCities);
+        this.setState({ targetCities }, () => {
+            this.getWeatherData(this.state.targetCities)
+
+        });
+
 
 
 
@@ -47,15 +72,19 @@ class Weathers extends Component {
     }
 
     render() {
-        // const { name: cityName, main: { temp } } = this.state.weather;
+
         return (
             <div>
                 <CitySearcher
                     onSearch={this.handleSearchCity}
                 />
-                <Card
-
-                />
+                {this.state.weathers.map(weather =>
+                    <Card
+                        key={weather.id}
+                        cityName={weather.name}
+                        temp={weather.main.temp}
+                    />
+                )}
             </div>
         );
     }
