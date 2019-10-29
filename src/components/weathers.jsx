dca97c3a9421c69ab7e1bf5131388dbf0187765d
Xingkai Yang <xingkaiyang@Xingkais-MacBook-Pro.local>
2019-10-29 12:45:02 -0400

    successfully get data from server
    setState() won't update the state immediately. Either call back function in setState() is used, or componentDidUpdate() is used.
    If lifecycle is use, you can set data inside, due to this function will be called every time the state is updated

--- src/components/weathers.jsx
+++ src/components/weathers.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import http from '../services/httpService';
 import Card from './common/card';
-import apiUrl from "../config.json";
+import { apiUrl, apiKey } from "../config.json";
 import CitySearcher from './citySearcher';
 import cities from '../city.list.json';
 import { toast } from 'react-toastify';
@@ -9,7 +9,19 @@ import { toast } from 'react-toastify';
 
 class Weathers extends Component {
     state = {
-        weather: {}
+        weathers: [],
+        targetCities: []
+    }
+
+
+
+
+    getWeatherData = async cities => {
+        const id = cities[0].id;
+        const { data } = await http.get(`${apiUrl}/data/2.5/weather?id=${id}&appid=${apiKey}`);
+        const weathers = [...this.state.weathers, data];
+        this.setState({ weathers });
+        console.log(this.state.weathers);
     }
 
 
@@ -21,7 +33,14 @@ class Weathers extends Component {
             .filter(c => c.country.toLowerCase() === countryName.toLowerCase());
         if (searchResults.length === 0)
             toast.error("Invalid City");
-        console.log(searchResults);
+
+        const targetCities = [...this.state.targetCities, ...searchResults];
+
+        this.setState({ targetCities }, () => { this.getWeatherData(this.state.targetCities) });
+
+
+
+
 
 
 
